794835b4acd264cbd5a6876b9f3fc2d44f40afa0
hansby <julian.hansby@takealot.com>
2025-07-31 12:41:04 +0200

    search, filtering and deleting logic updated for demo

--- angular-17-client/src/app/components/error-logs/error-logs.component.ts
+++ angular-17-client/src/app/components/error-logs/error-logs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService, searchType } from '../../services/search.service';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { of } from 'rxjs';
 @Component({
 	selector: 'error-logs',
 	templateUrl: './error-logs.component.html',
@@ -13,26 +14,27 @@ export class ErrorLogsComponent implements OnInit {
 	});	
 
 	currentResolveID: number | null = null;
+	searchText: string = '';
 
 	searchType = searchType;
 	_logResults: Array<any> = [];
-	_APILogs: Array<{data: any}> = [
-		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR' },
-		{ data: 'error on doc for askjdh 123iuohasf - 9928347235 - Passport ERROR' },
-		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR' },
-		{ data: 'error lkjsdf9 9234 supper - 9928347235 - Passport ERROR' },
-		{ data: 'error LOG (@#) - 9928347235 - TRUST ERROR' },
-		{ data: 'error on doc BUS REG DOC ERROR 28347235 - Passport ERROR' },
-		{ data: 'error on doc 2009 TRUST - 9928347235 - Passport ERROR' },
-		{ data: 'error on doc for julian IT Passport ERROR' },
-		{ data: 'error on WEB nsby - 9928347235 - Passport ERROR' },
-		{ data: 'error on doc ID DOC ERROR' },
-		{ data: 'error GOOGLE VERIFY LOG - Passport ERROR' },
-		{ data: 'error on doc kjhasd 8234 hansby - 9928347235 - Passport ERROR' },
-		{ data: 'error 5243 003999400 BUS REG DOC ERROR' },
-		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR' },
-		{ data: 'error on doc ID ERROR - Passport ERROR' },
-		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR' },
+	_APILogs: Array<{data: string, id: number}> = [
+		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR', id: 1 },
+		{ data: 'error on doc for askjdh 123iuohasf - 9928347235 - Passport ERROR', id: 2 },
+		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR', id: 3 },
+		{ data: 'error lkjsdf9 9234 supper - 9928347235 - Passport ERROR', id: 4 },
+		{ data: 'error LOG (@#) - 9928347235 - TRUST ERROR', id: 5 },
+		{ data: 'error on doc BUS REG DOC ERROR 28347235 - Passport ERROR', id: 6 },
+		{ data: 'error on doc 2009 TRUST - 9928347235 - Passport ERROR', id: 7 },
+		{ data: 'error on doc for julian IT Passport ERROR', id: 8 },
+		{ data: 'error on WEB nsby - 9928347235 - Passport ERROR', id: 9 },
+		{ data: 'error on doc ID DOC ERROR', id: 10 },
+		{ data: 'error GOOGLE VERIFY LOG - Passport ERROR', id: 11 },
+		{ data: 'error on doc kjhasd 8234 hansby - 9928347235 - Passport ERROR', id: 12 },
+		{ data: 'error 5243 003999400 BUS REG DOC ERROR', id: 13 },
+		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR', id: 14 },
+		{ data: 'error on doc ID ERROR - Passport ERROR', id: 15 },
+		{ data: 'error on doc for julian hansby - 9928347235 - Passport ERROR', id: 16 },
 	];
 	noResultsFound: boolean = false;
 
@@ -41,12 +43,17 @@ export class ErrorLogsComponent implements OnInit {
 	ngOnInit(): void {
 	}
 
+	get filteredItems() {
+		const lower = this.searchText.toLowerCase();
+		return this._APILogs.filter(filter => filter.data.toLowerCase().includes(lower));
+	}	
+
 	updateSearchBy(type: searchType) {
 		this.searchBy = type;
 	}
 
 	confirmResolve() {
-		this._APILogs = this._APILogs.filter((_:any, idx: number) => idx !== this.currentResolveID);
+		this._APILogs = this._APILogs.filter((_:any, idx: number) => _.id !== this.currentResolveID);
 	}
 
 	doSearch() {
@@ -54,18 +61,6 @@ export class ErrorLogsComponent implements OnInit {
 		this._logResults = [];
 		const keyword = this.form.controls['search'].value;
 		if (!keyword || keyword.length <= 0) return;
-		
-		/**
-		 * API Calls
-		 
-		this.search.getBySearchFilter(keyword).subscribe((searchResults) => {
-			console.log('lets see searchResults: ', searchResults);
-			if (searchResults.length > 0) {
-				this._UISearchResults = searchResults;
-			} else {
-				this.noResultsFound = true;
-			}
-		})*/
 	}
 
 }
